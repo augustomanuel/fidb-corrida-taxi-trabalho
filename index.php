35c825dfdfbf33528e4b4da3946d268dbcc51f72
Augusto Manauel <augustojoaomanuel05@gmail.com>
2026-03-15 18:30:16 +0100

    Declaração das variaveis normais e constantes

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+const TAXA_FIXA_KM = 100;
+const TAXA_FIM_SEMANA = 0.05;
+
+$nomeDoMotorista =  "Augusto";
+$nomeDoPassageiro = "Fernando";
+
+$pontoPartida = "Bairro Dunga";
+$pontoChegada = "Bairro Sonangol";
+
+$distancia_Km = 4.6;
+$fimDaSemana = true;
