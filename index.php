085ef01842b2441851d0136fc1409ca31483d489
Augusto Manauel <augustojoaomanuel05@gmail.com>
2026-03-15 18:31:58 +0100

    Criação da função Calcular Corrida

--- index.php
+++ index.php
@@ -11,3 +11,17 @@ $pontoChegada = "Bairro Sonangol";
 
 $distancia_Km = 4.6;
 $fimDaSemana = true;
+
+function CalcularCorrida(float $distancia_Km, $fimDaSemana ):float
+{
+    if($fimDaSemana===false)
+        {
+            $valorCorrida = $distancia_Km * TAXA_FIXA_KM;
+        }
+    else{
+        $valorCorrida = $distancia_Km * TAXA_FIXA_KM;
+        $valorCorrida = $valorCorrida + ($valorCorrida * TAXA_FIM_SEMANA);
+    }
+
+    return $valorCorrida;
+}
